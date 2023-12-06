881792732da902ee21d95c8e57e159afc75794bb
ChaeAg <dydkglel123@gmail.com>
2023-12-06 18:44:33 +0900

    feat: 6차 세미나 실습 코드

--- ThirdSeminar/src/main/java/com/server/dosopt/seminar/config/SecurityConfig.java
+++ ThirdSeminar/src/main/java/com/server/dosopt/seminar/config/SecurityConfig.java
@@ -1,24 +1,57 @@
 package com.server.dosopt.seminar.config;
 
+import com.server.dosopt.seminar.config.jwt.CustomAccessDeniedHandler;
+import com.server.dosopt.seminar.config.jwt.CustomJwtAuthenticationEntryPoint;
+import com.server.dosopt.seminar.config.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // 들어오는 요청에 대해서 어떻게 처리할지 ?
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+    private static final String[] AUTH_WHITELIST = {
+            "/sign-up",
+            "/sign-in"
+    };
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint, CustomAccessDeniedHandler customAccessDeniedHandler) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.customJwtAuthenticationEntryPoint = customJwtAuthenticationEntryPoint;
+        this.customAccessDeniedHandler = customAccessDeniedHandler;
+    }
+
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
+        return http
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+//                .sessionManagement()
+//                .sessionCreationPolicy(STATELESS)
+//                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(customJwtAuthenticationEntryPoint)
+                .accessDeniedHandler(customAccessDeniedHandler)
+                .and()
                 .authorizeHttpRequests()
-                .anyRequest().permitAll() // 모든 요청 다 받겠음
-                .and().build();
+                .requestMatchers(AUTH_WHITELIST).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
