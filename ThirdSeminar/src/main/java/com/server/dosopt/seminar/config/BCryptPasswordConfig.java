16e2c858cff2a6c54b8cd5afb0568c749c59d6f9
ChaeAg <dydkglel123@gmail.com>
2023-12-21 03:25:19 +0900

    CI/CD Test

--- ThirdSeminar/src/main/java/com/server/dosopt/seminar/config/BCryptPasswordConfig.java
+++ ThirdSeminar/src/main/java/com/server/dosopt/seminar/config/BCryptPasswordConfig.java
@@ -1,20 +1,20 @@
-package com.server.dosopt.seminar.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-public class BCryptPasswordConfig {
-
-    // salt 할 때 보안 강도를 어느정도로 할지 설정
-    // 높을수록 세지는데, 어느정도 높아지면 비슷함
-    // default가 10
-    private static final int STRENGTH = 10;
-
-    @Bean
-    public PasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder(STRENGTH);
-    }
-}
+//package com.server.dosopt.seminar.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//public class BCryptPasswordConfig {
+//
+//    // salt 할 때 보안 강도를 어느정도로 할지 설정
+//    // 높을수록 세지는데, 어느정도 높아지면 비슷함
+//    // default가 10
+//    private static final int STRENGTH = 10;
+//
+//    @Bean
+//    public PasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder(STRENGTH);
+//    }
+//}
--- ThirdSeminar/src/main/java/com/server/dosopt/seminar/config/SecurityConfig.java
+++ ThirdSeminar/src/main/java/com/server/dosopt/seminar/config/SecurityConfig.java
@@ -1,69 +1,69 @@
-package com.server.dosopt.seminar.config;
-
-import com.server.dosopt.seminar.config.jwt.CustomAccessDeniedHandler;
-import com.server.dosopt.seminar.config.jwt.CustomJwtAuthenticationEntryPoint;
-import com.server.dosopt.seminar.config.jwt.JwtAuthenticationFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint;
-    private final CustomAccessDeniedHandler customAccessDeniedHandler;
-
-    private static final String[] AUTH_WHITELIST = {
-            "/sign-up",
-            "/sign-in"
-    };
-
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint, CustomAccessDeniedHandler customAccessDeniedHandler) {
-        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-        this.customJwtAuthenticationEntryPoint = customJwtAuthenticationEntryPoint;
-        this.customAccessDeniedHandler = customAccessDeniedHandler;
-    }
-
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf().disable()
-                .formLogin().disable()
-                .httpBasic().disable()
-//                .sessionManagement()
-//                .sessionCreationPolicy(STATELESS)
+//package com.server.dosopt.seminar.config;
+//
+//import com.server.dosopt.seminar.config.jwt.CustomAccessDeniedHandler;
+//import com.server.dosopt.seminar.config.jwt.CustomJwtAuthenticationEntryPoint;
+//import com.server.dosopt.seminar.config.jwt.JwtAuthenticationFilter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+//    private final CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint;
+//    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+//
+//    private static final String[] AUTH_WHITELIST = {
+//            "/sign-up",
+//            "/sign-in"
+//    };
+//
+//    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint, CustomAccessDeniedHandler customAccessDeniedHandler) {
+//        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+//        this.customJwtAuthenticationEntryPoint = customJwtAuthenticationEntryPoint;
+//        this.customAccessDeniedHandler = customAccessDeniedHandler;
+//    }
+//
+//
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .csrf().disable()
+//                .formLogin().disable()
+//                .httpBasic().disable()
+////                .sessionManagement()
+////                .sessionCreationPolicy(STATELESS)
+////                .and()
+//                .exceptionHandling()
+//                .authenticationEntryPoint(customJwtAuthenticationEntryPoint)
+//                .accessDeniedHandler(customAccessDeniedHandler)
 //                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(customJwtAuthenticationEntryPoint)
-                .accessDeniedHandler(customAccessDeniedHandler)
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers(AUTH_WHITELIST).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedOriginPatterns("*")
-                        .allowedMethods("*");
-            }
-        };
-    }
-}
+//                .authorizeHttpRequests()
+//                .requestMatchers(AUTH_WHITELIST).permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+//                .build();
+//    }
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("*")
+//                        .allowedOriginPatterns("*")
+//                        .allowedMethods("*");
+//            }
+//        };
+//    }
+//}
